93b990e1035695202d5c669834ac36c57c320672
Pramod Kumar <50309120+kspramod@users.noreply.github.com>
2021-03-04 00:58:59 +1100

    test(design): updated snapshot tests

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,6 @@
-import logo from '../assets//logo.svg';
+import Dashboard from './containers/Dashboard';
+
+import logo from '../assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -6,13 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        Sample React Skeleton
       </header>
+      <Dashboard />
     </div>
   );
 }
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  test('Snapshot test', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders Sample React Skeleton `header`', () => {
+    render(<App />);
+    const pageHeader = screen.getByText(/Sample React Skeleton/i);
+    expect(pageHeader).toBeInTheDocument();
+  });
 });
