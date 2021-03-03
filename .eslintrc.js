e3be3382615de84f8893aed90616d50f65266866
Pramod Kumar <50309120+kspramod@users.noreply.github.com>
2021-03-03 23:33:48 +1100

    feat(design): diff --git a/.eslintignore b/.eslintignore

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// These rools are effective in root level
+module.exports = {
+  extends: ['react-app', 'eslint:recommended'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+};
---
+++ src/app/containers/Dashboard/index.jsx
@@ -0,0 +1,7 @@
+const Dashboard = () => (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+)
+
+export default Dashboard
